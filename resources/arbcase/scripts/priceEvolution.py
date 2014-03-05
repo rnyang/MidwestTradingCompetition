7cc06a19bec6337ca710b4534f9911c9c6eb45c0
Runnan Yang <yang.runnan@gmail.com>
2014-03-05 12:01:34 -0600

    updated price evol process

--- resources/arbcase/scripts/priceEvolution.py
+++ resources/arbcase/scripts/priceEvolution.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as ppt
 
 # 0. Time periods
-T = 100
+T = 1000
 
 ## A. Generate Book Updates
 
@@ -10,23 +10,23 @@ T = 100
 
 # AR(1) Process
 value = np.zeros(T)
-valuee = np.random.normal(0, 1.5, T)
+valuee = np.random.normal(0, 2.0, T)
 value[0] = 100.
 for i in xrange(1, T):
     value[i] = value[i-1] + valuee[i]
 
 # 2. Evolving Deviations from the True Value
 dev1 = np.zeros(T)
-deve1 = np.random.normal(0, 0.5, T)
+deve1 = np.random.normal(0, 1.0, T)
 dev2 = np.zeros(T)
-deve2 = np.random.normal(0, 0.5, T)
+deve2 = np.random.normal(0, 1.0, T)
 
 dev1[0] = 0
 dev2[0] = 0
 
 for i in xrange(1, T):
-	dev1[i] = 1.0 * ( deve1[i] + 0.5 * deve1[i-1] )
-	dev2[i] = 1.0 * ( deve2[i] + 0.5 * deve2[i-1] )
+	dev1[i] = 1.0 * ( deve1[i] + 0.75 * deve1[i-1] )
+	dev2[i] = 1.0 * ( deve2[i] + 0.75 * deve2[i-1] )
 
 ex1 = value + dev1
 ex2 = value + dev2
@@ -39,11 +39,11 @@ spread2 = np.ones(T)
 spreade2 = np.random.normal(1.0, 0.3, T)
 
 for i in xrange(2,T):
-	spread1[i] = 0.0 + 0.1 * np.abs(ex1[i] - ex1[i-1]) \
-	                 + 0.1 * np.abs(ex1[i] - ex1[i-2]) \
+	spread1[i] = 0.0 + 0.5 * np.abs(ex1[i] - ex1[i-1]) \
+	                 + 0.25 * np.abs(ex1[i] - ex1[i-2]) \
 	                 + np.abs(spreade1[i])
-	spread2[i] = 0.0 + 0.1 * np.abs(ex2[i] - ex2[i-1]) \
-	                 + 0.1 * np.abs(ex1[i] - ex1[i-2]) \
+	spread2[i] = 0.0 + 0.5 * np.abs(ex2[i] - ex2[i-1]) \
+	                 + 0.25 * np.abs(ex1[i] - ex1[i-2]) \
 	                 + np.abs(spreade2[i])
 
 ex1b = ex1 - spread1
@@ -64,14 +64,14 @@ grid = np.array(range(T))
 
 ## B. Generate Customer Orders
 
-# Evolving Variance
-# AR(1) Process
+# Variance of Customer Order Price
+# Moving Average Process
 
 custVar = np.zeros(T)
-custVare = np.random.normal(0, 1.5, T)
+custVare = np.random.normal(0, 0.5, T)
 custVar[0] = 1.
 for i in xrange(1, T):
-    custVar[i] = abs(custVar[i-1] + custVare[i])
+    custVar[i] = abs(custVare[i-1] + custVare[i])
 
 # Customer Orders come in around true value
 
@@ -80,8 +80,6 @@ custside = np.random.randint(2, size=T)
 
 # 0 = ROBOT, 1 = SNOW
 custexch = np.random.randint(2, size=T)
-
-print custexch
 
 custPrice = [np.random.normal(value[i], custVar[i]) for i in xrange(T)]
 custPrice = roundQtr(custPrice)
@@ -124,7 +122,7 @@ entries = []
 
 for i in xrange(T):
     if i % 5 != 0:
-        entry = "S," + str(1000. * i) + ",ORDER;"
+        entry = "S," + str(1000 * (i+1)) + ",ORDER;"
         if custexch[i] == 0:
             entry += "ROBOT;"
         elif custexch[i] == 1:
@@ -133,7 +131,7 @@ for i in xrange(T):
         entry += str(custPrice[i]) + "\n"
         entries.append(entry)
 
-    entries.append("S," + str(1000. * i) + ",BOOKUPDATE;ROBOT;" + str(ex1b[i]) + ";" + str(ex1a[i]) + ";SNOW;" + str(ex2b[i]) + ";" + str(ex2a[i]) + "\n")
+    entries.append("S," + str(1000 * (i+1)) + ",BOOKUPDATE;ROBOT;" + str(ex1b[i]) + ";" + str(ex1a[i]) + ";SNOW;" + str(ex2b[i]) + ";" + str(ex2a[i]) + "\n")
 
 with open('foo.csv', 'w') as openFile:
 	for entry in entries:
